bdf61199fa408731c60734787d9e98281ca92250
raihanat07 <raihanaqila70@gmail.com>
2021-10-08 20:23:23 +0700

    Initial commit

---
+++ upload_file/latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<html lang="en">
+
+<body>
+    <form enctype="multipart/form-data" method="post" action="latihan1_upload.php">
+        File yang diupload: <input type="file"name="fupload"><br>
+        Deskripsi File: <br><textarea name="deskripsi" rows="6" cols="20"></textarea><br>
+        <input type=submit value=Upload>
+    </form>
+    <br>
+    <a href="download.php">Download</a>
+</body>
