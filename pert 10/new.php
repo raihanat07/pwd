43b611622a1c241bb3410f039773b2c282c69478
raihanat07 <raihanaqila70@gmail.com>
2022-01-13 17:34:12 +0700

    d

---
+++ pert 10/new.php
@@ -0,0 +1,10 @@
+<?php
+require_once "../koneksi.php";
+$sql = "select * from mahasiswa where nim = '1800018093'";
+$query = mysqli_query($con, $sql);
+while ($row = mysqli_fetch_assoc($query)) {
+    $data[] = $row;
+}
+header('content-type: application/json');
+echo json_encode($data);
+mysqli_close($con);
